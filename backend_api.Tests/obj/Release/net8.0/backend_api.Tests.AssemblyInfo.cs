7ab4d95a540e900721fe4dbd5d1d42763030db16
Kavia AI <production@kavia.ai>
2026-03-26 10:01:57 +0000

    Checkpoint 3 - cm6f581d33

--- backend_api.Tests/obj/Release/net8.0/backend_api.Tests.AssemblyInfo.cs
+++ backend_api.Tests/obj/Release/net8.0/backend_api.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend_api.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3097dd4bf7e599d75aaf1cdefdde3570bb58c61e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9bf9e98d8d5808d67d2127b7dd0c97dc9368e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend_api.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend_api.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
